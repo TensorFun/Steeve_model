3c97d8e3896e0cbdcb90aa44fe7f3828dc9b8616
HanVincent <vincent.han.tw@gmail.com>
2018-04-19 16:00:22 +0800

    Fix some syntax error

--- SVM.py
+++ SVM.py
@@ -9,7 +9,7 @@ from pyfasttext import FastText
 
 class SVM:
     def __init__(self, TFIDF): # TODO: plz give me the model or using global singleton
-        self.model = FastText('wiki.en.bin')
+        self.model = FastText('/home/yee0/Atos/wiki.en.bin')
         self.TFIDF = TFIDF
         self.tfidf_dict, self.words = TFIDF.get_tfidf()
     
@@ -18,23 +18,23 @@ class SVM:
         self.tfidf_dict, self.words = TFIDF.get_tfidf()
     
     def to_feature(self, X):
-        new_X = [vec(self.TFIDF.predict_field(post), post) for post in X]
+        return [self.vec(self.TFIDF.predict_field(post), post) for post in X]
         
     def vec(self, field_index, post):
         v = np.zeros(300)
         post = set(post) # make unique
         for pl in post:
             if pl != '' and pl in self.words:
-                v += model.get_numpy_vector(pl) * tfidf_score[field_index][pl]
+                v += self.model.get_numpy_vector(pl) * self.tfidf_dict[field_index][pl]
         return v
     
     def train(self, X, y):
         # 建立 SVC 模型
         self.svc = svm.SVC()
-        svc_fit = self.svc.fit(to_feature(X), y)
+        svc_fit = self.svc.fit(self.to_feature(X), y)
 
     def predict(self, post):
-        return self.svc.predict(to_feature([post]))
+        return self.svc.predict(self.to_feature([post]))
         
 
 
